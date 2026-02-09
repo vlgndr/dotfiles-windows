851bc2d4dc7c8639771b9bc300ba0e031845dda4
vlgndr <gianlucanadir@gmail.com>
2026-02-09 14:15:36 +0100

    feat: remove wterminal settings.json and update powershell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,14 @@
-# Invoke-Expression (&starship init powershell)
+Set-Alias gvim  Get-VimCommand
+Set-Alias gwin  Get-WindowsCommand
+Set-Alias gmap  Get-WinLinuxMap
+Set-Alias gdock Get-DockerCommand
+Set-Alias grex  Get-RegexExample
+Set-Alias ggit  Get-GitCommand
+Set-Alias gdork Get-GoogleDorkingCommand
 
-Set-Alias -Name fa -Value Find-App
-Set-PSReadLineOption -PredictionSource None
+# Set-PSReadLineOption -PredictionSource None
+
+
+
 
 
